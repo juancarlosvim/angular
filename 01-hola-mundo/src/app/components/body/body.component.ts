cfc516b71767ec20a92408286a2b49626ebbadfa
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-08-23 17:19:39 +0200

    Aplicación básica de angular, utiliznado directivas *ngIf y *ngFor

---
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+  mostrar = true;
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran personalidad',
+    autor: 'Ben Parker'
+  };
+
+  personajes: string[] = ['Thor', 'Loki', 'Capitan America', 'Viuda Negra'];
+}
