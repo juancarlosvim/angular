68a30b8ea513971b4131dd19586a50719d2e7161
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-09-03 11:33:56 +0200

    02 - Spa - Componentes añadidos

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/shared/about/about.component';
+import { HeroesComponent } from './components/shared/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
