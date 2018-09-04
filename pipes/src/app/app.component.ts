718c070340845de029b125733102016740f4ba8b
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-09-04 11:41:50 +0200

    03 Pipes - uppercase, lowercase, slice

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  nombre = 'Juancarlosvim';
+  numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
